71d84a8192b57d0c5dbb239b7ac2f78bd56507fa
Jonasbadboys <jonathanjeanniard@sfr.fr>
2020-06-03 07:04:29 +0200

    add function & comment

--- src/InfoServer/InfoServer.php
+++ src/InfoServer/InfoServer.php
@@ -19,7 +19,6 @@ use phpseclib\Net\SSH2;
 class InfoServer
 {
     private $ssh;
-    private $values = [];
 
     public function __construct(SSH2 $_ssh)
     {
@@ -27,11 +26,15 @@ class InfoServer
     }
 
     /**
-     * Retourne les informations système
-     *
-     *
+     * Retourne les informations de base
+     *  Le hostname
+     *  L'os
+     *  La date
+     *  Le kernel
+     *  L'arch
+     * @return array
      */
-    public function getSystem()
+    public function getSystem(): array
     {
         $this->syteme = [
             'hostname' => trim($this->ssh->exec("hostname")),
@@ -40,26 +43,91 @@ class InfoServer
             'kernel' => trim($this->ssh->exec("uname -r")),
             'arch' => trim($this->ssh->exec("uname -m"))
         ];
-        return $this->values += $this->syteme;
-    }
-
-    public function getCpu(){
-        $this->user = trim($this->ssh->exec("id -un"));
-        $this->usageCpu = intval(trim($this->ssh->exec("ps -A -u ". $this->user ." -o pcpu | tail -n +2 | awk '{ usage += $1 } END { print usage }'")));
-        $this->nbrCores = intval(trim($this->ssh->exec("nproc")));
-        $this->cpu = [
-            'model' => trim($this->ssh->exec("cat /proc/cpuinfo | grep 'model name' | awk -F ':' '{print $2}' | head -n 1")),
-            'cores' => $this->nbrCores,
-            'usage' => round(($this->usageCpu/$this->nbrCores),2)
-        ];
-        return $this->values += $this->cpu;
+        return $this->syteme;
     }
 
     /**
+     * return news cpu
+     *  Model
+     *  Cores
+     *  Usage
+     * @return array
+     */
+    public function getCpu(){
+        $user = trim($this->ssh->exec("id -un"));
+        $usageCpu = intval(trim($this->ssh->exec("ps -A -u ". $user ." -o pcpu | tail -n +2 | awk '{ usage += $1 } END { print usage }'")));
+        $nbrCores = intval(trim($this->ssh->exec("nproc")));
+
+        $this->cpu = [
+            'model' => trim($this->ssh->exec("cat /proc/cpuinfo | grep 'model name' | awk -F ':' '{print $2}' | head -n 1")),
+            'cores' => $nbrCores,
+            'usage' => round(($usageCpu/$nbrCores),2)
+        ];
+        return $this->cpu;
+    }
+
+    /**
+     * return news ram
+     *  Used
+     *  Free
+     *  Total
+     *  Usage
      * @return array
      */
     public function getRam(): array
     {
-        return $this->values;
+        $ramUsed = intval(trim($this->ssh->exec("free -b | grep 'buffers/cache' | awk -F ':' '{print $2}' | awk '{print $1}'")));
+        $ramFree = intval(trim($this->ssh->exec("free -b | grep 'buffers/cache' | awk -F ':' '{print $2}' | awk '{print $2}'")));
+        $ramTotal = $ramUsed + $ramFree;
+
+        $this->ram = [
+            'used' => $ramUsed,
+            'free' => $ramFree,
+            'total' => $ramTotal,
+            'usage' => $ramUsage = round((($ramUsed / $ramTotal) * 100), 2),
+        ];
+
+        return $this->ram;
+    }
+
+    /**
+     * return array swap
+     *  Used
+     *  Free
+     *  Total
+     *  Usage
+     * @return array
+     */
+    public function swap(){
+        $swapUsage = 0;
+        $swapUsed = intval(trim($this->ssh->exec("free -b | grep 'Swap' | awk -F ':' '{print $2}' | awk '{print $2}'")));
+        $swapFree = intval(trim($this->ssh->exec("free -b | grep 'Swap' | awk -F ':' '{print $2}' | awk '{print $3}'")));
+        $swapTotal = $swapUsed + $swapFree;
+
+        $this->swap = [
+          'used' => $swapUsed,
+          'free' => $swapFree,
+          'Total' => $swapTotal,
+          'Usage' => $swapTotal!=0?round((($swapUsed / $swapTotal) * 100), 2) : $swapUsage
+        ];
+        return $this->swap;
+    }
+
+    /**
+     * return Load average
+     * @return array
+     */
+    public function getLoadAverage()
+    {
+        $loadavg = trim($this->ssh->exec("top -b -n 1 | grep 'load average' | awk -F ',' '{print $5}'"));
+        return $this->loadaverage = ['load average' => $loadavg];
+    }
+
+    public function getReseau(){
+
+    }
+
+    public function getDish(){
+
     }
 }
